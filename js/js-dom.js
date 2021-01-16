3de2cde9c7f7058232420b45fe360fea950c23b4
Grigoriy Demidovich <grig.demidovich@gmail.com>
2021-01-16 17:28:59 +0200

    wip

--- js/js-dom.js
+++ js/js-dom.js
@@ -1,3 +1,22 @@
-// https://github.com/goitacademy/javascript-homework/tree/master/homework-07
+//
+// Tasks https://github.com/goitacademy/javascript-homework/tree/master/homework-07
+// Test https://app.schoology.com/assignment/3151431576/assessment
+// Book http://fecore.net.ua/books/m5ph3r-javascript/module-07/
 
-// 
+console.log('window:', window);
+const fnNoStrict = function () {
+  console.log('this no strict:',this);
+};
+fnNoStrict();
+const fnStrict = function () {
+  'use strict';
+  console.log('this strict:', this);
+};
+fnStrict();
+
+console.log(window.document); // window писать не обязательно, т.е. console.log(window.document) === console.log(document);
+console.dir(window.document); // window писать не обязательно, т.е. console.dir(window.document) === console.dir(document);
+
+// поиск DOM узла
+const titleRef = document.querySelector('h1');
+console.log(titleRef);
