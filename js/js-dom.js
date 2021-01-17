9aa11759e8283304bd3da06a017eaebdbe11ea7e
Grigoriy Demidovich <grig.demidovich@gmail.com>
2021-01-17 08:09:05 +0200

    wip 7

--- js/js-dom.js
+++ js/js-dom.js
@@ -18,5 +18,18 @@ console.log(window.document); // window писать не обязательно
 console.dir(window.document); // window писать не обязательно, т.е. console.dir(window.document) === console.dir(document);
 
 // поиск DOM узла
-const titleRef = document.querySelector('h1');
+const titleRef = document.querySelector('h1'); // найдет первый попавшийся тег h1
 console.log(titleRef);
+console.dir(titleRef);
+
+const title2Ref = document.querySelector('.css-selector'); // найдет первый попавшийся тег с классом css-selector
+console.log(title2Ref);
+console.dir(title2Ref);
+
+const title2aRef = document.querySelectorAll('.css-selector'); // найдет все подходящие теги с классом css-selector
+console.log(title2aRef); // NodeList(3) [h2.css-selector, h2.css-selector, h2.css-selector]
+console.dir(title2aRef);
+
+titleRef.textContent = 'I am a JavaScripteaser'; // после изменения содержания страница перезагрузится
+
+//3200
