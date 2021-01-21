157a6f71247acbe23ee9fa78b0fe01df32ac05a8
Grigoriy Demidovich <grig.demidovich@gmail.com>
2021-01-21 20:19:41 +0200

    wip

--- js/task-1.js
+++ js/task-1.js
@@ -2,7 +2,7 @@
 // Посчитает и выведет в консоль количество категорий в ul#categories, то есть элементов li.item. Получится 'В списке 3 категории.'.
 
 const categoriesRef = document.querySelector('#categories'); // нашел элемент с id="categories"
-const categoriesItemRef = categoriesRef.querySelectorAll('.item'); // нашел все элементы с class="item"
+const categoriesItemRef = categoriesRef.querySelectorAll('li.item'); // нашел все элементы li с class="item"
 console.log(`В списке ${categoriesItemRef.length} категории.`);
 
 // Для каждого элемента li.item в списке ul#categories, найдет и выведет в консоль текст заголовка элемента (тега h2) и количество элементов в категории (всех вложенных в него элементов li).
