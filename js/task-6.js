d933f2a4b359226b91c3045bd05d6e477948a3a8
Grigoriy Demidovich <grig.demidovich@gmail.com>
2021-02-01 15:43:13 +0200

    task-6 done

--- js/task-6.js
+++ js/task-6.js
@@ -1,11 +1,22 @@
-// Напиши скрипт, который бы при потере фокуса на инпуте, 
+// Напиши скрипт, который бы при потере фокуса на инпуте,
 // проверял его содержимое на правильное количество символов.
 // Сколько символов должно быть в инпуте, указывается в его атрибуте data-length.
 // Если введено подходящее количество, то border инпута становится зеленым, если неправильное - красным.
 // Для добавления стилей, используй CSS-классы valid и invalid.
 
-// change event 
-// dataset.length
+const inputRef = document.querySelector('#validation-input');
 
-// elemName.classList.add('className');
-// elemName.classList.remove('className');
+inputRef.addEventListener('change', verifyInputHandler);
+inputRef.addEventListener('focus', cleanOnFocusHandler);
+
+function verifyInputHandler() {
+  if (event.target.value.length === parseInt(inputRef.dataset.length)) {
+    inputRef.classList.add('valid');
+  } else {
+    inputRef.classList.add('invalid');
+  }
+}
+
+function cleanOnFocusHandler() {
+  inputRef.classList.remove('valid', 'invalid');
+}
